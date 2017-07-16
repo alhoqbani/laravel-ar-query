51f78a7d0ad0294d51ef3b71ca208491058fb941
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-07-16 02:50:32 -0700

    clean balde file

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="list-group" :posts="{{$posts->toJson()}}">
-        <posts :posts="{{$posts->toJson()}}"></posts>
-    </div>
+    <posts :posts="{{$posts->toJson()}}"></posts>
 @endsection
