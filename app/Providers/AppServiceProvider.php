13275906163ddb38bd4408295310271e42483fa6
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-02-05 11:37:11 -0800

    upgrade to laravel 5.5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Events\QueryExecuted;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    
     /**
      * Bootstrap any application services.
      *
@@ -16,12 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \DB::listen(function (QueryExecuted $query) {
-            //
-        });
-        Schema::defaultStringLength(191);
+        //
     }
-    
+
     /**
      * Register any application services.
      *
