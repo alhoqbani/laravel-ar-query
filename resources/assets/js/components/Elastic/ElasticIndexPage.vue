22ba106195b90cf70cd850fdab6dc2435051ba25
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-02-05 19:06:05 -0800

    install elasticsearch js client

--- resources/assets/js/components/Elastic/ElasticIndexPage.vue
+++ resources/assets/js/components/Elastic/ElasticIndexPage.vue
@@ -2,6 +2,21 @@
     <h1 class="text-center">{{ title }}</h1>
 </template>
 <script>
+    const elasticsearch = require('elasticsearch');
+    const client = new elasticsearch.Client({
+        host: 'localhost:9200',
+        log: 'trace'
+    });
+
+    client.ping({
+        requestTimeout: 30000,
+    }, function (error) {
+        if (error) {
+            console.error('elasticsearch cluster is down!');
+        } else {
+            console.log('All is well');
+        }
+    });
 
     export default {
         name: 'elastic-index-page',
